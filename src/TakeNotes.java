95d7bdd109adba5ef5797dfc1a593728401f3aee
Nguyen Duc Anh <ducanh @gmail.com>
2026-04-22 08:24:59 +0700

    Hoàn thành bài 4

---
+++ src/TakeNotes.java
@@ -0,0 +1,16 @@
+public class TakeNotes {
+
+    private String name = "John";
+    private String classes = "C02";
+
+    public TakeNotes() {
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ src/TestTakeNotes.java
@@ -0,0 +1,10 @@
+public class TestTakeNotes {
+    public static void main(String[] args) {
+        TakeNotes takenotes = new TakeNotes();
+
+        takenotes.setName("Alice");
+        takenotes.setClasses("C03");
+
+        System.out.println("Đã cập nhật thông tin sinh viên thành công!");
+    }
+}
